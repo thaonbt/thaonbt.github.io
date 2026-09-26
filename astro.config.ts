41bb40b99f6077d1b4e3ae0aab7b6821e9c01f8a
Thao Nguyen <nb2t71@gmail.com>
2026-09-26 08:24:41 +0700

    fix: set base path to root for consistent deployment

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ import config from "./astro-paper.config";
 
 export default defineConfig({
   site: config.site.url,
-  base: process.env.NODE_ENV === "production" ? "" : "/",
+  base: '/',
   integrations: [
     mdx(),
     sitemap({
